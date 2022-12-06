7614ef95d58df9e80f03cdd67e8df8dcef422cc7
Jason Pemberton <jrpembe@mac.com>
2022-12-06 10:51:32 -0700

    First Commit

---
+++ SAIT_Projects/Lab3/classification.py
@@ -0,0 +1,73 @@
+# %% read data
+import pandas as pd
+
+train = pd.read_csv("titanic/train.csv")
+test = pd.read_csv("titanic/test.csv")
+
+
+# %% checkout out first few rows
+train.head()
+
+
+# %% checkout out dataframe info
+train.info()
+
+
+# %% describe the dataframe
+train.describe(include="all")
+
+
+# %% visualize the dataset, starting with the Survied distribution
+import seaborn as sns
+
+sns.countplot(x="Survived", data=train)
+
+
+# %% Survived w.r.t Pclass / Sex / Embarked ?
+
+
+# %% Age distribution ?
+
+
+# %% Survived w.r.t Age distribution ?
+
+
+# %% SibSp / Parch distribution ?
+
+
+# %% Survived w.r.t SibSp / Parch  ?
+
+
+# %% Dummy Classifier
+from sklearn.dummy import DummyClassifier
+from sklearn.metrics import f1_score
+
+
+def evaluate(clf, x, y):
+    pred = clf.predict(x)
+    result = f1_score(y, pred)
+    return f"F1 score: {result:.3f}"
+
+
+dummy_clf = DummyClassifier(random_state=2020)
+
+dummy_selected_columns = ["Pclass"]
+dummy_train_x = train[dummy_selected_columns]
+dummy_train_y = train["Survived"]
+
+dummy_clf.fit(dummy_train_x, dummy_train_y)
+print("Training Set Performance")
+print(evaluate(dummy_clf, dummy_train_x, dummy_train_y))
+
+truth = pd.read_csv("truth_titanic.csv")
+dummy_test_x = test[dummy_selected_columns]
+dummy_test_y = truth["Survived"]
+
+print("Test Set Performance")
+print(evaluate(dummy_clf, dummy_test_x, dummy_test_y))
+
+print("Can you do better than a dummy classifier?")
+
+
+# %% Your solution to this classification problem
+
---
+++ SAIT_Projects/Lab3/regression.py
@@ -0,0 +1,68 @@
+# %% read data
+import pandas as pd
+
+train = pd.read_csv(
+    "house-prices-advanced-regression-techniques/train.csv"
+)
+test = pd.read_csv(
+    "house-prices-advanced-regression-techniques/test.csv"
+)
+
+
+# %% checkout out first few rows
+train.head()
+
+
+# %% checkout out dataframe info
+train.info()
+
+
+# %% describe the dataframe
+train.describe(include="all")
+
+
+# %% SalePrice distribution
+import seaborn as sns
+
+sns.distplot(train["SalePrice"])
+
+
+# %% SalePrice distribution w.r.t CentralAir / OverallQual / BldgType / etc
+
+
+# %% SalePrice distribution w.r.t YearBuilt / Neighborhood
+
+
+# %%
+from sklearn.dummy import DummyRegressor
+from sklearn.metrics import mean_squared_log_error
+import numpy as np
+
+
+def evaluate(reg, x, y):
+    pred = reg.predict(x)
+    result = np.sqrt(mean_squared_log_error(y, pred))
+    return f"RMSLE score: {result:.3f}"
+
+
+dummy_reg = DummyRegressor()
+
+dummy_selected_columns = ["MSSubClass"]
+dummy_train_x = train[dummy_selected_columns]
+dummy_train_y = train["SalePrice"]
+
+dummy_reg.fit(dummy_train_x, dummy_train_y)
+print("Training Set Performance")
+print(evaluate(dummy_reg, dummy_train_x, dummy_train_y))
+
+truth = pd.read_csv("truth_house_prices.csv")
+dummy_test_x = test[dummy_selected_columns]
+dummy_test_y = truth["SalePrice"]
+
+print("Test Set Performance")
+print(evaluate(dummy_reg, dummy_test_x, dummy_test_y))
+
+print("Can you do better than a dummy regressor?")
+
+
+# %% your solution to the regression problem
